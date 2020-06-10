8abf3d2f415d9da25b870a11396f414a07df4430
Walter Remache <walter.remache@fractalsoft.ec>
2020-06-10 17:13:24 -0500

    Desplegar en heroku

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/ides-ui>'));
+app.use(express.static(path.resolve(__dirname, '/dist/ides-ui')));
 
 app.get('*', function(req, res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname + '/dist/ides-ui/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/ides-ui/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Servidor desplegado');
+});
