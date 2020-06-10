6830d8a670d58af723718aba89353fb228c4e77c
Walter Remache <walter.remache@fractalsoft.ec>
2020-06-10 17:40:28 -0500

    Desplegar en heroku v3

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ides-ui'));
 
 app.get('/*', function(req, res) {
@@ -12,5 +10,4 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/ide-iu/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000);
