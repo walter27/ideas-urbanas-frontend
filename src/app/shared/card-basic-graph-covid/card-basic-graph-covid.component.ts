2125972bc58fbd06970b46f6b6742cc398d58ded
Walter Remache <walter.remache@fractalsoft.ec>
2020-08-05 09:21:33 -0500

    Implemntacion de redes sosiales v0

--- src/app/shared/card-basic-graph-covid/card-basic-graph-covid.component.ts
+++ src/app/shared/card-basic-graph-covid/card-basic-graph-covid.component.ts
@@ -4,6 +4,7 @@ import HC_exporting from 'highcharts/modules/exporting';
 import HC_export from 'highcharts/modules/export-data';
 import Color_Axis from 'highcharts/modules/coloraxis';
 import Series_Label from 'highcharts/modules/series-label';
+import { Facebook } from 'angular-feather/icons';
 
 
 
@@ -17,11 +18,12 @@ export class CardBasicGraphCovidComponent implements OnInit, OnChanges {
   highcharts: any;
   updateDemo: boolean;
   chartOptions: any = {};
+  socialMedia: any = [];
+  imageURL: string;
 
   @Input("dates") dates: any[];
   @Input("data") data: any[];
   @Input('variable') variable: any;
-
 
 
   constructor() {
@@ -109,5 +111,107 @@ export class CardBasicGraphCovidComponent implements OnInit, OnChanges {
 
     this.updateDemo = true;
 
+    this.getURLImage();
+
   }
+
+
+  getURLImage() {
+
+    let chart = this.highcharts.charts[0];
+    let ohlcSvg = chart.getSVG(this.chartOptions);
+    let urlExport = this.highcharts.getOptions().exporting.url;
+
+    //console.log(this.highcharts.getOptions().exporting);
+
+    let data = {
+      options: JSON.stringify(this.chartOptions),
+      filename: 'test.png',
+      type: 'image/png',
+      async: true
+    };
+
+    let that = this;
+
+
+    $.post(urlExport, data, function (url) {
+      that.socialMedia = [];
+      this.imageURL = urlExport + url;
+
+      that.socialMedia.push({
+        name: 'Facebook',
+        link: `https://www.facebook.com/sharer.php?u=${this.imageURL}`,
+        img: 'social-facebook'
+
+      });
+
+      that.socialMedia.push({
+        name: 'Twitter',
+        link: `https://twitter.com/intent/tweet?url=${this.imageURL}&text=Plataforma de Ideas Urbanas`,
+        img: 'social-twitter'
+
+      });
+
+
+
+
+      /*var urlCreator = window.URL || window.webkitURL
+      document.querySelector("#image").src = imageUrl
+      fetch(imageUrl).then(response => response.blob()).then(data => {
+        // You have access to chart data here
+        //console.log(data)
+      })*/
+    });
+
+
+
+
+    //console.log(ohlcSvg);
+    //console.log(this.highcharts.getOptions().exporting.url);
+    //console.log(this.chartOptions);
+
+
+
+
+
+    /*$('#facebook').click(function () {
+
+
+      function serialize(obj) {
+        return Object.keys(obj).map(function (p) {
+          return encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]);
+        }).join("&");
+      }
+
+      function postToFacebook(url) {
+        console.log(url);
+        let url2 = urlExport + url;
+        console.log(url2);
+
+        /*const title = 'Laboratorio';
+        window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(url2) + '&t=' +
+          encodeURIComponent(title), 'sharer', 'toolbar=0,status=0,width=626,height=436');
+      }
+
+
+      $.ajax({
+        type: 'POST',
+        data: serialize({
+          svg: ohlcSvg,
+          type: 'image/png',
+          async: true
+        }),
+        url: urlExport,
+        success: postToFacebook,
+        error: function (e) {
+          throw e;
+        }
+      });
+    });*/
+
+  }
+
 }
+
+
+
